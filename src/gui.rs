ec93bcbc19ef130cec6571aa7aeb93cd9fe52654
nyx <nnyyxxxx@protonmail.com>
2024-10-11 22:08:12 -0400

    add debug prints and implement menu

--- src/gui.rs
+++ src/gui.rs
@@ -1,118 +1,249 @@
-use std::{cell::RefCell, collections::HashMap, rc::Rc};
-
+use gtk::glib;
+use gtk::prelude::*;
 use gtk::{
-    prelude::*, Application, ApplicationWindow, Box, Button, CheckButton, FileChooserAction,
-    FileChooserDialog, FileFilter, HeaderBar, Notebook, Orientation, ResponseType,
+    Application, ApplicationWindow, Box, Button, CheckButton, Entry, Label, Orientation, Stack,
+    StackSidebar, Widget,
 };
 
-#[derive(Clone)]
+use hyprland_parser::HyprlandConfig;
+use std::collections::HashMap;
+
 pub struct ConfigGUI {
     pub window: ApplicationWindow,
-    box_: Box,
-    header_bar: HeaderBar,
+    config_widgets: HashMap<String, ConfigWidget>,
     pub open_button: Button,
-}
-
-pub struct ConfigWidget {
-    config_path: String,
-    option_checkboxes: HashMap<String, CheckButton>,
+    pub save_button: Button,
+    content_box: Box,
 }
 
 impl ConfigGUI {
     pub fn new(app: &Application) -> Self {
         let window = ApplicationWindow::builder()
             .application(app)
-            .title("Hyprland Configuration GUI")
+            .title("Hyprland Configuration")
             .default_width(800)
             .default_height(600)
             .build();
 
-        let box_ = Box::builder()
-            .orientation(Orientation::Vertical)
-            .spacing(0)
-            .build();
+        let main_box = Box::new(Orientation::Vertical, 0);
+        let open_button = Button::with_label("Open Config");
+        let save_button = Button::with_label("Save");
+        save_button.set_visible(false);
 
-        let header_bar = HeaderBar::builder()
-            .show_title_buttons(false)
-            .decoration_layout("")
-            .build();
+        let button_box = Box::new(Orientation::Horizontal, 5);
+        button_box.set_halign(gtk::Align::Center);
+        button_box.append(&open_button);
+        button_box.append(&save_button);
 
-        let open_button = Button::builder().label("Open Configuration File").build();
+        main_box.append(&button_box);
 
-        header_bar.set_title_widget(Some(&open_button));
+        let content_box = Box::new(Orientation::Horizontal, 0);
+        main_box.append(&content_box);
 
-        window.set_titlebar(Some(&header_bar));
-        window.set_child(Some(&box_));
+        window.set_child(Some(&main_box));
 
-        Self {
+        let config_widgets = HashMap::new();
+
+        ConfigGUI {
             window,
-            box_,
-            header_bar,
+            config_widgets,
             open_button,
+            save_button,
+            content_box,
         }
     }
 
-    pub fn open_config_file<F>(&self, callback: Rc<RefCell<F>>)
+    pub fn hide_config_options(&mut self) {
+        self.content_box.set_visible(false);
+        self.save_button.set_visible(false);
+    }
+
+    pub fn show_config_options(&mut self) {
+        self.content_box.set_visible(true);
+        self.save_button.set_visible(true);
+    }
+
+    pub fn load_config(&mut self, config: &HyprlandConfig) {
+        self.config_widgets.clear();
+        self.content_box.set_visible(true);
+
+        let stack = Stack::new();
+        let sidebar = StackSidebar::new();
+        sidebar.set_stack(&stack);
+
+        self.content_box.append(&sidebar);
+        self.content_box.append(&stack);
+
+        for category in &[
+            "General",
+            "Decoration",
+            "Animations",
+            "Input",
+            "Gestures",
+        ] {
+            let widget = ConfigWidget::new(category);
+            stack.add_titled(&widget.container, Some(category), category);
+            self.config_widgets.insert(category.to_string(), widget);
+        }
+
+        for (category, widget) in &self.config_widgets {
+            widget.load_config(config, category);
+        }
+    }
+
+    pub fn save_config(&self) -> HyprlandConfig {
+        let mut config = HyprlandConfig::new();
+        for (category, widget) in &self.config_widgets {
+            widget.save_config(&mut config, category);
+        }
+        config
+    }
+
+    pub fn open_config_file<F>(&self, callback: F)
     where
         F: Fn(String) + 'static,
     {
-        let dialog = FileChooserDialog::builder()
-            .title("Open Hyprland Configuration File")
-            .action(FileChooserAction::Open)
-            .transient_for(&self.window)
-            .modal(true)
-            .build();
+        println!("open_config_file method called");
+        let file_chooser = gtk::FileChooserDialog::new(
+            Some("Open Config File"),
+            Some(&self.window),
+            gtk::FileChooserAction::Open,
+            &[
+                ("Cancel", gtk::ResponseType::Cancel),
+                ("Open", gtk::ResponseType::Accept),
+            ],
+        );
 
-        dialog.add_button("Cancel", ResponseType::Cancel);
-        dialog.add_button("Open", ResponseType::Accept);
+        println!("FileChooserDialog created");
 
-        let filter = FileFilter::new();
-        filter.set_name(Some("Configuration files"));
-        filter.add_pattern("*.conf");
+        file_chooser.set_modal(true);
 
-        dialog.add_filter(&filter);
-
-        dialog.connect_response({
-            let callback = callback.clone();
-            move |dialog, response| {
-                match response {
-                    ResponseType::Accept => {
-                        if let Some(file) = dialog.file() {
-                            if let Some(filename) = file.path() {
-                                let callback = callback.borrow();
-                                callback(filename.to_string_lossy().to_string());
-                            }
+        file_chooser.connect_response(move |dialog, response| {
+            println!("File chooser response: {:?}", response);
+            if response == gtk::ResponseType::Accept {
+                if let Some(file) = dialog.file() {
+                    if let Some(path) = file.path() {
+                        if let Some(path_str) = path.to_str() {
+                            println!("File selected: {}", path_str);
+                            callback(path_str.to_string());
                         }
                     }
-                    ResponseType::Cancel => {
-                        println!("canceled");
-                    }
-                    _ => {}
                 }
-                dialog.close();
             }
+            dialog.close();
         });
 
-        dialog.show();
-    }
+        println!("About to show file chooser");
+        file_chooser.show();
+        println!("File chooser show() called");
 
-    pub fn load_file(&self, file_path: String) {
-        self.window.set_title(Some("ok"));
+        glib::timeout_add_local(std::time::Duration::from_millis(100), move || {
+            if file_chooser.is_visible() {
+                glib::ControlFlow::Continue
+            } else {
+                glib::ControlFlow::Break
+            }
+        });
     }
 }
 
-impl ConfigWidget {
-    pub fn new(config_path: String) -> Self {
-        let widget = ConfigWidget {
-            config_path,
-            option_checkboxes: HashMap::new(),
-        };
+pub struct ConfigWidget {
+    container: Box,
+    options: HashMap<String, Widget>,
+}
 
-        widget.build_ui();
-        widget
+impl ConfigWidget {
+    fn new(category: &str) -> Self {
+        let container = Box::new(Orientation::Vertical, 10);
+        container.set_margin_start(20);
+        container.set_margin_end(20);
+        container.set_margin_top(20);
+        container.set_margin_bottom(20);
+
+        let mut options = HashMap::new();
+
+        match category {
+            "General" => {
+                Self::add_int_option(&container, &mut options, "border_size", "Border Size");
+                Self::add_bool_option(
+                    &container,
+                    &mut options,
+                    "no_border_on_floating",
+                    "No Border on Floating",
+                );
+                Self::add_int_option(&container, &mut options, "gaps_in", "Gaps In");
+                Self::add_int_option(&container, &mut options, "gaps_out", "Gaps Out");
+            }
+            _ => {}
+        }
+
+        ConfigWidget { container, options }
     }
 
-    fn build_ui(&self) {
-        let notebook = Notebook::new();
+    fn add_int_option(
+        container: &Box,
+        options: &mut HashMap<String, Widget>,
+        name: &str,
+        label: &str,
+    ) {
+        let hbox = Box::new(Orientation::Horizontal, 5);
+        let label = Label::new(Some(label));
+        let entry = Entry::new();
+
+        hbox.append(&label);
+        hbox.append(&entry);
+        container.append(&hbox);
+
+        options.insert(name.to_string(), entry.upcast());
+    }
+
+    fn add_bool_option(
+        container: &Box,
+        options: &mut HashMap<String, Widget>,
+        name: &str,
+        label: &str,
+    ) {
+        let checkbox = CheckButton::with_label(label);
+        container.append(&checkbox);
+        options.insert(name.to_string(), checkbox.upcast());
+    }
+
+    fn load_config(&self, config: &HyprlandConfig, category: &str) {
+        for (name, widget) in &self.options {
+            let value = self.extract_value(config, category, name);
+            if let Some(entry_widget) = widget.downcast_ref::<Entry>() {
+                entry_widget.set_text(&value);
+            } else if let Some(checkbox) = widget.downcast_ref::<CheckButton>() {
+                checkbox.set_active(value == "true");
+            }
+        }
+    }
+
+    fn extract_value(&self, config: &HyprlandConfig, _category: &str, name: &str) -> String {
+        let config_str = config.to_string();
+        for line in config_str.lines() {
+            if line.trim().starts_with(&format!("{} = ", name)) {
+                return line
+                    .split('=')
+                    .nth(1)
+                    .map(|s| s.trim().to_string())
+                    .unwrap_or_default();
+            }
+        }
+        String::new()
+    }
+
+    fn save_config(&self, config: &mut HyprlandConfig, category: &str) {
+        for (name, widget) in &self.options {
+            let value = if let Some(entry) = widget.downcast_ref::<Entry>() {
+                entry.text().to_string()
+            } else if let Some(checkbox) = widget.downcast_ref::<CheckButton>() {
+                checkbox.is_active().to_string()
+            } else {
+                continue;
+            };
+            let entry = format!("{} = {}", name, value);
+            config.add_entry(category, &entry);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,50 @@
+use gtk::prelude::*;
+use gtk::Application;
+use hyprland_parser::parse_config;
+use std::fs;
 use std::{cell::RefCell, rc::Rc};
-
-use gtk::{prelude::*, Application};
 
 mod gui;
 
-use gui::{ConfigGUI, ConfigWidget};
-
-const APP_ID: &str = "nnyyxxxx.hyprgui";
-
-fn main() -> gtk::glib::ExitCode {
-    let app = Application::builder().application_id(APP_ID).build();
+fn main() {
+    let app = Application::builder()
+        .application_id("nnyyxxxx.hyprgui")
+        .build();
 
     app.connect_activate(build_ui);
-    app.run()
+    app.run();
 }
 
 fn build_ui(app: &Application) {
-    let gui = ConfigGUI::new(app);
-    gui.window.present();
+    let gui = Rc::new(RefCell::new(gui::ConfigGUI::new(app)));
 
-    let callback = Rc::new(RefCell::new({
-        let gui = gui.clone();
-        move |filename: String| {
-            gui.load_file(filename);
-        }
-    }));
+    gui.borrow_mut().hide_config_options();
 
-    gui.open_button.clone().connect_clicked(move |_| {
-        gui.open_config_file(callback.clone());
+    let gui_clone = gui.clone();
+    gui.borrow().open_button.connect_clicked(move |_| {
+        println!("Open button clicked");
+        open_config_file(gui_clone.clone());
     });
+
+    let gui_clone = gui.clone();
+    gui.borrow().save_button.connect_clicked(move |_| {
+        let config = gui_clone.borrow().save_config();
+        let updated_config_str = config.to_string();
+        println!("Saving configuration:\n{}", updated_config_str);
+    });
+
+    gui.borrow().window.present();
 }
 
-/* use hyprland_parser::parse_config;
-use std::fs;
-use std::path::PathBuf;
-
-fn main() {
-    let home = std::env::var("HOME").unwrap();
-    let config_path = PathBuf::from(home).join(".config/hypr/hyprland.conf");
-
-    let config_str = fs::read_to_string(&config_path).unwrap();
-
-    let mut parsed_config = parse_config(&config_str);
-
-    parsed_config.add_entry("decoration", "rounding = 10");
-    parsed_config.add_entry("decoration.blur", "enabled = true");
-    parsed_config.add_entry("decoration.blur", "size = 10");
-
-    let updated_config_str = parsed_config.to_string();
-
-    fs::write(&config_path, updated_config_str).unwrap();
-
-    println!("Updated hyprland.conf with new configurations."); */
+fn open_config_file(gui: Rc<RefCell<gui::ConfigGUI>>) {
+    println!("open_config_file function called");
+    let gui_clone = gui.clone();
+    gui.borrow().open_config_file(move |path| {
+        println!("Callback executed with path: {}", path);
+        let config_str = fs::read_to_string(&path).unwrap();
+        let parsed_config = parse_config(&config_str);
+        gui_clone.borrow_mut().load_config(&parsed_config);
+        gui_clone.borrow_mut().show_config_options();
+    });
+    println!("open_config_file function completed");
+}
