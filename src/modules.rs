3e4510da7bdcfcc3090f9078542fad6c878c9e03
nyx <nnyyxxxx@protonmail.com>
2024-11-05 23:02:07 -0500

    nest shadow modules

--- src/modules.rs
+++ src/modules.rs
@@ -197,56 +197,42 @@ impl ConfigWidget {
                 WidgetBuilder::add_bool_option(
                     &container,
                     &mut options,
-                    "drop_shadow",
-                    "Drop Shadow",
-                    "enable drop shadows on windows",
-                );
-                WidgetBuilder::add_int_option(
-                    &container,
-                    &mut options,
-                    "shadow_range",
-                    "Shadow Range",
-                    "Shadow range (\"size\") in layout px",
-                );
-                WidgetBuilder::add_int_option(
-                    &container,
-                    &mut options,
-                    "shadow_render_power",
-                    "Shadow Render Power",
-                    "in what power to render the falloff (more power, the faster the falloff) [1 - 4]",
+                    "shadow:sharp",
+                    "Shadow Sharp",
+                    "if enabled, will make the shadows sharp, akin to an infinite render power",
                 );
                 WidgetBuilder::add_bool_option(
                     &container,
                     &mut options,
-                    "shadow_ignore_window",
+                    "shadow:ignore_window",
                     "Shadow Ignore Window",
                     "if true, the shadow will not be rendered behind the window itself, only around it.",
                 );
                 WidgetBuilder::add_color_option(
                     &container,
                     &mut options,
-                    "col.shadow",
+                    "shadow:color",
                     "Shadow Color",
                     "shadow's color. Alpha dictates shadow's opacity.",
                 );
                 WidgetBuilder::add_color_option(
                     &container,
                     &mut options,
-                    "col.shadow_inactive",
+                    "shadow:color_inactive",
                     "Inactive Shadow Color",
-                    "inactive shadow color. (if not set, will fall back to col.shadow)",
+                    "inactive shadow color. (if not set, will fall back to shadow:color)",
                 );
                 WidgetBuilder::add_string_option(
                     &container,
                     &mut options,
-                    "shadow_offset",
+                    "shadow:offset",
                     "Shadow Offset",
                     "shadow's rendering offset. Format: \"x y\" (e.g. \"0 0\")",
                 );
                 WidgetBuilder::add_float_option(
                     &container,
                     &mut options,
-                    "shadow_scale",
+                    "shadow:scale",
                     "Shadow Scale",
                     "shadow's scale. [0.0 - 1.0]",
                 );
